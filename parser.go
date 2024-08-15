58670298f92916d5a6ae01b5d2bc22515036e83c
vzvu3k6k <vzvu3k6k@gmail.com>
2024-08-16 00:24:22 +0900

    Support photos in div.profile_container

--- parser.go
+++ parser.go
@@ -30,21 +30,21 @@ func parseLm2(body string) (*User, []*Photo, error) {
 
 	photos := []*Photo{}
 	var parseErr error
-	doc.Find("div.photos-feed-item").EachWithBreak(func(_ int, s *goquery.Selection) bool {
+	doc.Find(`[id^="imgcontainersecondary-"]`).EachWithBreak(func(_ int, s *goquery.Selection) bool {
 		id, ok := s.Attr("id")
 		if !ok {
 			parseErr = errors.New("id is not found")
 			return false
 		}
-		imageID := id[13:] // "imgcontainer-123" -> "123"
+		imageID := id[22:] // "imgcontainersecondary-123" -> "123"
 
-		src, ok := s.Find(".photos-feed-image").First().Attr("src")
+		src, ok := s.Find(".photos-feed-image-container, .photos-feed-image").First().Attr("src")
 		if !ok {
 			parseErr = errors.New("src is not found")
 		}
 
 		photos = append(photos, &Photo{
-			Title:    s.Find(".photos-feed-data-name").First().Text(),
+			Title:    s.Find(".photos-feed-data-title-container, .photos-feed-data-name").First().Text(),
 			PageURL:  fmt.Sprintf("https://1x.com/photo/%s", imageID),
 			ImageURL: src,
 		})
